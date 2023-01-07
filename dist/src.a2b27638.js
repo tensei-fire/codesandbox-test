3b0ef2e1d30eb8667e9cd8a80fdf68a95c1306c2
tensei <tensei0703@icloud.com>
2023-01-07 08:27:44 +0000

    20230107_テンプレート文字列

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -167,6 +167,73 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * const,let等の変数宣言
+ */
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数の再宣言可能
+// var val1 = "var変数の再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+//const val3 = "const変数";
+//console.log(val3);
+
+//const変数は上書き不可能
+//val3 = "const変数上書き";
+
+//const変数は再宣言不可能
+//const val3 = "const変数を再宣言";
+
+//constで定義したobjectはプロパティの変更可能
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// val4.name="jak";
+// val4.address="hiroshima";
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ *
+ */
+
+var name = "じゃけぇ";
+var age = "28";
+//「私の名前はじゃけぇです。年齢は28歳です。」
+
+//従来の方法
+var message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+var message2 = "\u79C1\u306E\u540D\u524D\u306F".concat(name, "\u3067\u3059\u3002\u5E74\u9F62\u306F").concat(age, "\u3067\u3059\u3002");
+console.log(message2);
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -47,3 +47,70 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * const,let等の変数宣言
+ */
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数の再宣言可能
+// var val1 = "var変数の再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+//const val3 = "const変数";
+//console.log(val3);
+
+//const変数は上書き不可能
+//val3 = "const変数上書き";
+
+//const変数は再宣言不可能
+//const val3 = "const変数を再宣言";
+
+//constで定義したobjectはプロパティの変更可能
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// val4.name="jak";
+// val4.address="hiroshima";
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ *
+ */
+
+const name = "じゃけぇ";
+const age = "28";
+//「私の名前はじゃけぇです。年齢は28歳です。」
+
+//従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+//テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
