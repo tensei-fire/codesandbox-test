d26dc094f35bfee50532c92a38909b1cbc3f1c88
tensei <tensei0703@icloud.com>
2023-01-08 08:07:15 +0000

    20230108_分割代入

--- src/index.js
+++ src/index.js
@@ -145,3 +145,29 @@ const func3 = (num1, num2) => {
 //const func3 = (num1, num2) => num1 + num2;
 
 console.log(func3(10, 20));
+
+// /**
+//  *
+//  * 分割代入
+//  */
+
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です`;
+// console.log(message2);
+
+const myProfile = ["じゃけぇ", 28];
+
+const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+console.log(message3);
+//順番で格納される↓
+const [name, age] = myProfile;
+const message4 = `名前は${name}です。年齢は${age}歳です`;
+console.log(message4);
